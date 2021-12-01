e778cc9e5a9580ede0333ff5c667888ae2f5c748
Thomas Tramley <thomas@caddieservice.co>
2021-12-01 15:38:39 -0400

    Add lisp solution

---
+++ day1.lisp
@@ -0,0 +1,21 @@
+;; part 1
+(let ((inputs (map 'list #'parse-integer (uiop:read-file-lines "day1.input")))
+      (current 0))
+  (reduce (lambda (x y) (progn
+                            (if (> y x)
+                                (setf current (1+ current)))
+                            y))
+            inputs)
+  (print (concatenate 'string  "Part 1: " (write-to-string current))))
+
+(let ((inputs (map 'list #'parse-integer (uiop:read-file-lines "day1.input")))
+      (current 0)
+      (previous nil))
+  (loop for i from 3 upto (length inputs) do
+    (let* ((window (subseq inputs (- i 3) i))
+           (window-sum (reduce #'+ window)))
+      (progn
+        (if (and previous (> window-sum previous))
+            (setf current (1+ current)))
+        (setf previous window-sum))))
+  (print (concatenate 'string  "Part 2: " (write-to-string current))))
