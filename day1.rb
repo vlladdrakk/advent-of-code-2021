2f1858fae2cd6aa5d0f672e247ef029e499b4e51
Thomas Tramley <thomas@caddieservice.co>
2021-12-01 14:24:30 -0400

    Day 1 solutions

---
+++ day1.rb
@@ -0,0 +1,34 @@
+require 'prettyprint'
+
+current = nil
+previous = nil
+increases = 0
+
+File.readlines('day1.input').each do |line|
+  current = line.to_i
+  increases += 1 if previous && current > previous
+
+  previous = current
+end
+
+puts "Problem 1: #{increases}"
+
+window = []
+current = nil
+previous = nil
+increases = 0
+
+File.readlines('day1.input').each do |line|
+  window.push(line.to_i)
+  next if window.count < 3
+
+  current = window.last(3).reduce(&:+)
+
+  if previous && current > previous
+    increases += 1
+  end
+
+  previous = current
+end
+
+puts "Problem 2: #{increases}"
